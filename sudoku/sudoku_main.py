fadc0476d25beb8ff614920813f6bcce14ddc04a
Evan Banerjee <ebanerjee22@gmail.com>
2022-06-20 13:50:30 -0400

    finish adding functions to cell class.

--- sudoku/sudoku_main.py
+++ sudoku/sudoku_main.py
@@ -1,3 +1,5 @@
+import pygame as p
+
 class Cell:
     #row and col start with index 0
     def __init__(self, row, col, sector):
@@ -6,6 +8,26 @@ class Cell:
 
         #sector takes the form "xy" where x is row and y is col
         self.sector = str((row // 3) + 1) + str((col // 3) + 1)
-        
+
         self.value = None
         self.pos_values = []
+
+    #for explicitly setting values
+    def set_value(self, value):
+        self.value = value
+
+    #for adding possible values
+    def add_pos_value(self, value):
+        if value not in self.pos_values:
+            self.pos_values.append(value)
+
+            #to make it easier we sort the list of possible values
+            self.pos_values.sort()
+    
+    #to easily remove possible values from the list
+    def remove_pos_value(self, value):
+        if value in self.pos_values:
+            self.pos_values.remove(value)
+
+            #to make it easier we sort the list of possible values
+            self.pos_values.sort()
