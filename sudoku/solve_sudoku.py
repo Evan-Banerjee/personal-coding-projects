e10eaee7b543118b351cf924f77c42e9de3a2b1d
Evan Banerjee <ebanerjee22@gmail.com>
2022-06-21 11:42:26 -0400

    implement and test user input

--- sudoku/solve_sudoku.py
+++ sudoku/solve_sudoku.py
@@ -2,6 +2,7 @@ import pygame as p
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
 
 #pygame setup
 p.init()
@@ -32,5 +33,18 @@ while True:
                 if (i % 3 == 0):
                     p.draw.line(screen, BLACK, (10, i * 80 + 10), (730, i * 80 + 10), width=6)
 
+            #find row and col where user clicked
+            if event.type == p.MOUSEBUTTONUP:
+                coordinates = p.mouse.get_pos()
+                col = (coordinates[0] - 10) // 80
+                row = (coordinates[1] - 10) // 80
+
+                #test validity of row and col
+                rect_x_start = (col * 80) + 11
+                rect_y_start = (row * 80) + 11
+
+                #draw square where user clicked
+                coordinates_square = (rect_x_start, rect_y_start, 79, 79)
+                p.draw.rect(screen, GREEN, p.Rect(coordinates_square),)
 
         p.display.update()
