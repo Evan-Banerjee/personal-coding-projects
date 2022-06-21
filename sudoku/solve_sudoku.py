522fcbf2597c4ff6e0f41bde73bbef5362f2a4c4
Evan Banerjee <ebanerjee22@gmail.com>
2022-06-21 11:00:40 -0400

    basic pygame setup

--- sudoku/solve_sudoku.py
+++ sudoku/solve_sudoku.py
@@ -1,1 +1,15 @@
-print("test")
+import pygame as p
+
+WHITE = (255, 255, 255)
+
+p.init()
+screen = p.display.set_mode((720, 720))
+screen.fill(WHITE)
+p.display.set_caption("sudoku solver")
+running = True
+
+while running:
+    for event in p.event.get():
+        if event.type == p.QUIT:
+            running = False
+    p.display.update()
